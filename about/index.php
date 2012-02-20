491f82412963b64d5c7537baed0dec2c185bbdbb
Tyler Graf <tylergraf@tyler.local>
2012-02-19 22:52:12 -0700

    moving to laptop.

--- about/index.php
+++ about/index.php
@@ -1,43 +1,68 @@
 <?php
-  $title = 'Design';
-  $page = 'design';
+  $title = 'About';
+  $page = 'about';
   $nav = 'header';
 
   include_once('../api/dribbble/controller.php');
   include('../includes/_header.php');
 
 ?>
+<style type="text/css">
+  .page-skin-ext-about img.main-image {width:100%; margin-top: 15px;}
 
+</style>
 <section class="line">
-  <div class="gallery">
-    <?php
-//      print_r($shots);
-      foreach ($shots as $shot){ ?>
-        <div class="unit image-grid">
-          <div class="image-wrapper">
-            <?php echo '<img src="'.$shot['image_url'].'">'; ?>
-            <h2><?php echo '<img src="'.$shot['title'].'">'; ?></h2>
-          </div>
-        </div>
-    <?php } ?>
+  <div class="unit size1of2">
+    <img class="main-image" src="https://fbcdn-sphotos-a.akamaihd.net/hphotos-ak-ash4/229690_10150407165394896_710659895_10868232_2251572_n.jpg" alt="Tyler Graf">
+  </div>
+  <div class="unit size1of2">
+    <p>Me? I&#39;m a goofball. I&#39;m sarcastic, fun and witty, but I can be serious when I need to be. I care about things. I care about people. The most important thing is the user and the fact that semantic code and tasteful design make a better experience is just a side effect. I&#39;m super attentive to details and I love doing things the right way.</p>
+    <p>I&#39;ve been into web for years. I worked at overstock.com as a web designer and shifted to front&ndash;end development. Working for a billion dollar company completely opened my paradigm helping me understand how to function in the real world of large corporations.</p>
+    <p>I currently work for a start&ndash;up called Candeo as Director of Technology where I wear literally 10 hats. Spreading thin isn&#39;t what I necessarily want or like but it stretches and pushes me to learn new skills.</p>
 
- </div>
+
+  </div>
+
+
 </section>
-
+<section class="line">
+  <div class="unit">
+    <h3>Skills</h3>
+    <ul>
+      <li>html</li>
+      <li>css</li>
+      <li>jQuery</li>
+      <li>Taste.</li>
+      <li>Fashion</li>
+      <li>stuff</li>
+      <li>Golf</li>
+      <li>Photography</li>
+      <li>JavaScript</li>
+      <li>Ruby on Rails</li>
+      <li>php</li>
+      <li>Design</li>
+      <li>UX</li>
+      <li>Filmmaking</li>
+      <li>agile</li>
+      <li>linux</li>
+      <li>Objective&ndash;C</li>
+      <li>Motion Graphics</li>
+    </ul>
+  </div>
+</section>
+<section class="line">
+  <div class="unit">
+    <div class="media media-skin-about-quote">
+      <div class="media-block"><img src="https://lh3.googleusercontent.com/-ri66JNGW2eA/TlPTLLBOIuI/AAAAAAAAApU/4vFANsFmdxc/s872/inkt.jpg" alt="Joe Fawkes"></div>
+      <div class="media-body">
+        <p class="ext-italic">&ldquo;He&#39;s just some Tyler kid. Some infinitely genuine Tyler kid. If you did some math and took genuine to the power of infinity the answer&#39;d be Tyler, or g<sup>&#8734;</sup>=tg. His genuineosity encompasses everything he does: whether it be javascript, photography or dating, he is all in/full on.&rdquo;</p>
+        <p>&mdash; Joe Fawkes</p>
+      </div>
+    </div>
+  </div>
+</section>
 <script type="text/javascript">
-  function resizeImg(_this){
-    var parentWidth = _this.parent().width();
-    _this.parent().height(parentWidth*2/3);
-  }
 
-  $(document).ready(function(){
-    var imgs = $('img');
-    imgs.load(function(){
-      resizeImg($(this));
-      $(this).fadeIn();
-    });
-    $(window).resize(function(){resizeImg($('img'))});
-  });
 
 </script>
 
