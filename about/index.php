23cd02c477a3e3b3d83fbaa4841dae49cc9268ce
Tyler Graf <tylergraf@gmail.com>
2012-05-09 09:45:51 -0600

    updating for bio.

--- about/index.php
+++ about/index.php
@@ -16,40 +16,39 @@
     <img class="main-image" src="https://fbcdn-sphotos-a.akamaihd.net/hphotos-ak-ash4/229690_10150407165394896_710659895_10868232_2251572_n.jpg" alt="Tyler Graf">
   </div>
   <div class="unit size1of2">
-    <p>Me? I&#39;m a goofball. I&#39;m sarcastic, fun and witty, but I can be serious when I need to be. I care about things. I care about people. The most important thing is the user and the fact that semantic code and tasteful design make a better experience is just a side effect. I&#39;m super attentive to details and I love doing things the right way.</p>
-    <p>I&#39;ve been into web for years. I worked at overstock.com as a web designer and shifted to front&ndash;end development. Working for a billion dollar company completely opened my paradigm helping me understand how to function in the real world of large corporations.</p>
-    <p>I currently work for a start&ndash;up called Candeo as Director of Technology where I wear literally 10 hats. Spreading thin isn&#39;t what I necessarily want or like but it stretches and pushes me to learn new skills.</p>
-
+<!--    <p>Me? I&#39;m a goofball. I&#39;m sarcastic, fun and witty, but I can be serious when I need to be. I care about things. I care about people. The most important thing is the user and the fact that semantic code and tasteful design make a better experience is just a side effect. I&#39;m super attentive to details and I love doing things the right way.</p>-->
+<!--    <p>I&#39;ve been into web for years. I worked at overstock.com as a web designer and shifted to front&ndash;end development. Working for a billion dollar company completely opened my paradigm helping me understand how to function in the real world of large corporations.</p>-->
+<!--    <p>I currently work for a start&ndash;up called Candeo as Director of Technology where I wear literally 10 hats. Spreading thin isn&#39;t what I necessarily want or like but it stretches and pushes me to learn new skills.</p>-->
+    <p>My name is Tyler Graf. I&#39;m twenty&ndash;six, I live in South Jordan, and I&#39;m a Mac; reliable, efficient, and evolving. I love my family. I love my faith and served an LDS mission in Leon, Mexico. I love working with computers and creating new things. I&#39;ve been blessed with an innate desire to learn new skills which means I am continually working to develop myself personally and professionally. I have been a front&ndash;end developer for more than three years and have learned to adapt to diverse workplaces, starting with freelance work, moving to a billion dollar company, and, most recently, working with a startup company. I am a creator, a problem solver, and a maven.</p>
 
   </div>
 
 
 </section>
-<section class="line">
-  <div class="unit">
-    <h3>Skills</h3>
-    <ul>
-      <li>html</li>
-      <li>css</li>
-      <li>jQuery</li>
-      <li>Taste.</li>
-      <li>Fashion</li>
-      <li>stuff</li>
-      <li>Golf</li>
-      <li>Photography</li>
-      <li>JavaScript</li>
-      <li>Ruby on Rails</li>
-      <li>php</li>
-      <li>Design</li>
-      <li>UX</li>
-      <li>Filmmaking</li>
-      <li>agile</li>
-      <li>linux</li>
-      <li>Objective&ndash;C</li>
-      <li>Motion Graphics</li>
-    </ul>
-  </div>
-</section>
+<!--<section class="line">-->
+<!--  <div class="size1of1">-->
+<!--    <h3>Skills</h3>-->
+<!--    <ul class="list-skin-skills">-->
+<!--      <li class="list-item" data-skill-level="100"><span>html</span></li>-->
+<!--      <li class="list-item" data-skill-level="100"><span>css</span></li>-->
+<!--      <li class="list-item" data-skill-level="90"><span>jQuery</span></li>-->
+<!--      <li class="list-item" data-skill-level="70"><span>JavaScript</span></li>-->
+<!--      <li class="list-item" data-skill-level="60"><span>Ruby on Rails</span></li>-->
+<!--      <li class="list-item" data-skill-level="80"><span>UX</span></li>-->
+<!--      <li class="list-item" data-skill-level="67"><span>php</span></li>-->
+<!--      <li class="list-item" data-skill-level="40"><span>Objective&ndash;C</span></li>-->
+<!--      <li class="list-item" data-skill-level="90"><span>Agile</span></li>-->
+<!--      <li class="list-item" data-skill-level="65"><span>linux</span></li>-->
+<!--      <li class="list-item" data-skill-level="80"><span>Design</span></li>-->
+<!--      <li class="list-item" data-skill-level="100"><span>Photography</span></li>-->
+<!--      <li class="list-item" data-skill-level="100"><span>Filmmaking</span></li>-->
+<!--      <li class="list-item" data-skill-level="80"><span>Motion Graphics</span></li>-->
+<!--      <li class="list-item" data-skill-level="100"><span>Taste.</span></li>-->
+<!--      <li class="list-item" data-skill-level="100"><span>Stuff</span></li>-->
+<!--      <li class="list-item" data-skill-level="100"><span>Golf</span></li>-->
+<!--    </ul>-->
+<!--  </div>-->
+<!--</section>-->
 <section class="line">
   <div class="unit">
     <div class="media media-skin-about-quote">
@@ -61,9 +60,6 @@
     </div>
   </div>
 </section>
-<script type="text/javascript">
 
-
-</script>
 
 <?php include('../includes/_footer.php');?>
