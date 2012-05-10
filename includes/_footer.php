a520d460eff63b6dbb540cc90b0ab66440a7afc7
Tyler Graf <tylergraf@gmail.com>
2012-05-09 18:02:56 -0600

    fixing homepage.

--- includes/_footer.php
+++ includes/_footer.php
@@ -17,22 +17,57 @@
       e.preventDefault();
       window.location = $(this).find('a').attr('href');
     });
-    function menuAnimate(){
-      $('.circles').toggleClass('on');
-      $('.nav-init').toggleClass('on');
-      if($('.circles').hasClass('on')){
-        setTimeout("$('.circles').css('z-index','1')",500);
-      }
-      else{
-        $('.circles').css('z-index','-1');
-      }
+
+
+    var _nav = $('.nav-init');
+    var _circles = $('.circles');
+
+
+    function menuDeAnimate() {
+      _nav.addClass('on');
+      _circles.addClass('on');
+      setTimeout("$('.circles').css('z-index','1')", 500);
+      _nav.animate({
+        'margin-left': '0',
+        'margin-top': '0',
+        'width': '150px',
+        'height': '150px'
+      }, 500);
+      _nav.find('h2').animate({
+        'padding-top': '60px'
+      }, 500);
+
     }
-    $('.nav-init').click(function(){menuAnimate()});
-    $('.menu-item').click(function(){menuAnimate()});
+
+    function menuAnimate() {
+      _circles.removeClass('on');
+      _nav.removeClass('on');
+      _circles.css('z-index', '-1');
+      _nav.animate({
+        'margin-left': '-25px',
+        'margin-top': '-25px',
+        'width': '200px',
+        'height': '200px'
+      }, 500);
+      _nav.find('h2').animate({
+        'padding-top': '85px'
+      }, 500);
+
+    }
+
+    _nav.toggle(
+      function() {
+        menuDeAnimate();
+      },
+      function() {
+        menuAnimate();
+      }
+    );
 
 //    init transitions
     $('.page-header h1').addClass('on');
-    $('.nav-init').addClass('on');
+    menuAnimate();
+
   });
 </script>
 </div>
